bbd1f77a62d8f95113746d90620e5de4b8833501
nazimfeni <mdnazimuddin77@gmail.com>
2024-08-12 23:53:39 +0600

    setup API using axios

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,17 +2,40 @@
     <img class="logo" src="../assets/logo.png" alt="" srcset="" >
 <h1>Sign Up</h1>
 <div class="register">
-<input type="text" placeholder="Enter Name" />
-<input type="email" placeholder="Enter Email" />
-<input type="password" placeholder="Enter Password" />
-<button>Sign Up</button>
+<input type="text" v-model="name" placeholder="Enter Name" />
+<input type="email" v-model="email" placeholder="Enter Email" />
+<input type="password" v-model="password" placeholder="Enter Password" />
+<button v-on:click="signUp">Sign Up</button>
 </div>
 </template>
 
 <script>
-
+import axios from 'axios';
 export default{
-    name: 'SignUp'
+    name: 'SignUp',
+
+    data(){
+        return{
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+        methods: {
+            async signUp(){
+                console.warn("Signup", this.name, this.email, this.password)
+                let result = await axios.post("http://localhost:3000/users",{
+                    name:this.name,
+                    email:this.email,
+                    password:this.password
+                });
+                console.warn(result);
+                if(result.status==201){
+                    alert("Sign Up done")
+                }
+            }
+        }
+   
 
 }
 
