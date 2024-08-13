3cdcd491ed974a39038361267123d42d1b9c5990
nazimjrf <nazim@jrfintel.com>
2024-08-13 16:05:21 +0600

    Create Login component

---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+
+<img class="logo" src="../assets/logo.png" alt="" srcset="" >
+<h1>Login</h1>     
+<div class="login">
+
+<input type="email" v-model="email" placeholder="Enter Email" />
+<input type="password" v-model="password" placeholder="Enter Password" />
+<button v-on:click="signUp">Login</button>
+<p>
+    <router-link to="/sign-up">Sign Up</router-link>
+</p>
+</div>    
+</template>
+
+<script>
+export default{
+      // eslint-disable-next-line vue/multi-word-component-names
+      name: 'Login',
+
+}
+</script>
+
+
+<style>
+
+</style>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -6,6 +6,9 @@
 <input type="email" v-model="email" placeholder="Enter Email" />
 <input type="password" v-model="password" placeholder="Enter Password" />
 <button v-on:click="signUp">Sign Up</button>
+<p>
+    <router-link to="/login">Login</router-link>
+</p>
 </div>
 </template>
 
@@ -53,27 +56,4 @@ export default{
 </script>
 
 <style>
-.logo{
-    width: 100px;
-}
-
-.register input {
-    width: 300px;
-    height: 40px;
-    padding-left: 20px;
-    display: block;
-    margin-bottom: 30px;
-    margin-left: auto;
-    margin-right: auto;
-    border: 1px solid skyblue;
-}
-
-.register button {
-    width: 320px;
-    height: 40px;
-    border: 1px solid skyblue;
-    color: #fff;
-    background-color: skyblue;
-    cursor: pointer;
-}
 </style>
